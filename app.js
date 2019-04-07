e51b0ed499d3e2a13c44594d2a06b2f329944a8b
xMidNightFTW <41029448+xMidNightFTW@users.noreply.github.com>
2019-04-07 06:41:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels.`);
+    client.user.setActivity(config.status);
+});
+
+client.login(config.token);
